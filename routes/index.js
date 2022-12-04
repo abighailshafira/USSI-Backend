0878bf0415cddeb20027bf99f4cabdff9f50acd8
Abighail Shafira Ihsani <abighailshafira@upi.edu>
2022-12-04 15:47:42 +0700

    Update

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const { register, login, logout, getInstitution, getAdmin, updateAdmin, deleteAdmin, getAdminById } = require("../controller/authController");
-const { registration } = require("../controller/registrationController");
 const { getAllTraining, createTraining, detailTraining, detailTrainingById, updateTraining, deleteTraining } = require("../controller/trainingController");
-const { getAllEvent, postCertificate } = require("../controller/eventController");
 const { getAllInstitution, createInstitution, deleteInstitution, updateInstitution, getInstitutionById } = require("../controller/institutionController");
+const { getAllEvent, postCertificate } = require("../controller/eventController");
+const { registration } = require("../controller/registrationController");
 
 let storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -20,27 +20,38 @@ let storage = multer.diskStorage({
 
 let upload = multer({ storage: storage });
 
+// Auth
 router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", logout);
 router.get("/admin", getAdmin);
 router.get("/admin/:id", getAdminById);
 router.put("/admin/:id", updateAdmin);
 router.delete("/admin/:id", deleteAdmin);
-router.post("/login", login);
-router.post("/logout", logout);
+
+// Lembaga
+router.post("/institution", createInstitution);
+router.get("/institution", getAllInstitution);
+router.get("/institution/:id", getInstitutionById);
+router.put("/institution/:id", updateInstitution);
+router.delete("/institution/:id", deleteInstitution);
+
 router.get("/manuk/:id", getInstitution);
+
+// Pendaftaran
 router.post("/registration", upload.single("image"), registration);
-router.get("/training", getAllTraining);
+
+// Pelatihan
 router.post("/training", createTraining);
 router.get("/detail/training", detailTraining);
 router.get("/detail/training/:id", detailTrainingById);
 router.put("/detail/training/:id", upload.single("img"), updateTraining);
 router.delete("/detail/training/:id", deleteTraining);
+
+// Kegiatan
 router.get("/event", getAllEvent);
 router.post("/event", upload.single("certificate"), postCertificate);
-router.get("/institution", getAllInstitution);
-router.get("/institution/:id", getInstitutionById);
-router.post("/institution", createInstitution);
-router.delete("/institution/:id", deleteInstitution);
-router.put("/institution/:id", updateInstitution);
+
+// router.get("/training", getAllTraining);
 
 module.exports = router;
